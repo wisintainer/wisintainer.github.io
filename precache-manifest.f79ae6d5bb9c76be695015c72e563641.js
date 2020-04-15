111f77a6958bd194706898f34d2c1dbb025b91af
Adriano <wisintainer90@gmail.com>
2020-04-14 22:37:21 -0300

    inicial

---
+++ precache-manifest.f79ae6d5bb9c76be695015c72e563641.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16bb6a38278253970349f1aced50a3be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78292a31251aef7d6d0a",
+    "url": "/static/css/main.c2e2b41b.chunk.css"
+  },
+  {
+    "revision": "c39941edbe8be072de82",
+    "url": "/static/js/2.658ca48e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.658ca48e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78292a31251aef7d6d0a",
+    "url": "/static/js/main.dd555b4d.chunk.js"
+  },
+  {
+    "revision": "bd1815a99857e00c9f50",
+    "url": "/static/js/runtime-main.b96f4cb3.js"
+  }
+]);
